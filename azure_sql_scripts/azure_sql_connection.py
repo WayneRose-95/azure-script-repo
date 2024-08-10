87324451cc19a886f3336797442e9df24cc84ff5
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-08-10 20:01:48 +0100

    Refactoring azure_sql_connection script to a class for reusability purposes

--- azure_sql_scripts/azure_sql_connection.py
+++ azure_sql_scripts/azure_sql_connection.py
@@ -1,27 +1,54 @@
 import pyodbc
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.exc import OperationalError
 import pandas as pd 
 
-server = '' # replace with your Azure SQL Database server name
-database= '' # replace with your Azure SQL Database name
-username = '' # replace with your Azure SQL Database username
-password = '' # replace with your Azure SQL Database password
-driver = '{ODBC Driver 18 for SQL Server}'
+class AzureSQLDatabaseConnector:
 
-# Create the connection string
-connection_string=f'Driver={driver};\
-    Server=tcp:{server},1433;\
-    Database={database};\
-    Uid={username};\
-    Pwd={password};\
-    Encrypt=yes;\
-    TrustServerCertificate=no;\
-    Connection Timeout=30;'
+    def __init__(
+            self, 
+            server=None,
+            database=None, 
+            username=None, 
+            password=None, 
+            driver='{ODBC Driver 18 for SQL Server}'
+            ):
+        self.server = server # replace with your Azure SQL Database server name
+        self.database = database # replace with your Azure SQL Database name
+        self.username = username # replace with your Azure SQL Database username
+        self.password = password # replace with your Azure SQL Database password
+        self.driver = '{ODBC Driver 18 for SQL Server}'
+        pass 
 
-# Create the engine to connect to the database
-engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(connection_string))
+    def create_connection_string(self):
+        # Create the connection string
+        connection_string = f'Driver={self.driver};\
+            Server=tcp:{self.server},1433;\
+            Database={self.database};\
+            Uid={self.username};\
+            Pwd={self.password};\
+            Encrypt=yes;\
+            TrustServerCertificate=no;\
+            Connection Timeout=30;'
+        
+        return connection_string
 
-engine.connect() 
+    def initalise_database_engine(self, connection_string : str):
+        # Create the engine to connect to the database
+        engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(connection_string))
+        return engine 
+        
 
-print("connection successful")
+    def test_connection(self, engine : Engine):
+        try:
+            engine.connect()
+            print('Connection Successful')
+        except OperationalError:
+            print('Failed to establish connection to database')
+            raise OperationalError
+        
+
+if __name__ == "__main__":
+    azdb = AzureSQLDatabaseConnector()
 
