9bb58a3ba8e2c7ba58d80112fcd8ad9ddfa8e4fe
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-08-10 21:42:04 +0100

    abstracting methods from azure_storage.py script

--- azure_blob_storage/azure_storage.py
+++ azure_blob_storage/azure_storage.py
@@ -18,71 +18,36 @@ class AzureBlobStorageConnector:
         connection_string = f"DefaultEndpointsProtocol=https;AccountName={self.account_name};AccountKey={self.account_key};EndpointSuffix=core.windows.net"
         return connection_string
 
-    # def create_blob_service_client(self, connection_string : str): 
-        
-    #     return blob_service_client 
-
-    def retrieve_data_from_blob(
-            self, 
-            connection_string : str,
-            container_name : str, 
-            blob_name : str, 
-            file_name : str 
-            ):
+    def create_blob_service_client(self, connection_string : str): 
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        container_name = container_name 
-        blob_name = blob_name 
+        return blob_service_client 
+
+    def retrieve_blob_client(
+            self, 
+            blob_service_client : BlobServiceClient,
+            container_name : str, 
+            blob_name : str
+            ):
+
         blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
         print(f"Retrieving {blob_name} from {container_name}")
-
-        download_stream = blob_client.download_blob()
-        blob_content = download_stream.readall()
-        print(blob_content.decode('utf-8'))
-
-        print('Blob Downloaded successfully')
-
+        return blob_client 
+    
+    def retrieve_data_from_blob(
+            self, 
+            blob_client : BlobClient,
+            file_name : str 
+            ):
+    
         with open(file_name, 'w+') as file:
             download_stream = blob_client.download_blob()
             blob_content = download_stream.readall()  
             contents = blob_content.decode('utf-8') 
             file.write(contents) 
-        
 
-    # def download_file(self, file_name : str, blob_client : BlobClient): 
+        print(f'Blob Downloaded successfully. Saved as {file_name}')
 
-    #     with open(file_name, 'w+') as file:
-    #         download_stream = blob_client.download_blob()
-    #         blob_content = download_stream.readall()  
-    #         contents = blob_content.decode('utf-8') 
-    #         file.write(contents) 
-
-
-# account_name = '' # Replace with the name of your azure storage account 
-# account_key = '' # Replace with your account key. You can find this under Security + Networking > Access Keys 
-# connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
-# blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-# print("Success")
-
-
-# container_name = '' # Replace with the name of your container 
-# blob_name = '' # Replace with the name of your file. This should contain the file name itself alongside its extension e.g. file.txt
-# blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-
-# print(f"Retrieving {blob_name} from {container_name}")
-
-# download_stream = blob_client.download_blob()
-# blob_content = download_stream.readall()
-# print(blob_content.decode('utf-8'))
-
-# print("Blob downloaded successfully")
-
-# # Writing the blob to a file 
-# # Add the writing mode 
-# with open('downloaded_file.txt', 'w+') as file:
-#     download_stream = blob_client.download_blob()
-#     blob_content = download_stream.readall()  
-#     contents = blob_content.decode('utf-8') 
-#     file.write(contents) 
+if __name__ == "__main__":
+    azblobconnector = AzureBlobStorageConnector()
 
