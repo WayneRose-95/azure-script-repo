9f8a309936ef3a965e3563191f1f884c3ecb6984
WayneRose-95 <e.guishardrose@hotmail.co.uk>
2024-09-30 22:15:22 +0100

    Adding method to create a container inside a blob

--- azure_blob_storage/azure_storage.py
+++ azure_blob_storage/azure_storage.py
@@ -1,6 +1,8 @@
 from azure.storage.blob import BlobServiceClient
 from azure.storage.blob import BlobClient
 from azure.storage.blob import ContainerClient
+from azure.core.exceptions import ResourceExistsError
+from azure.core.exceptions import ResourceNotFoundError
 
 class AzureBlobStorageConnector:
     
@@ -23,9 +25,23 @@ class AzureBlobStorageConnector:
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         return blob_service_client 
     
-    def create_container_client(connection_string : str, container_name : str):
+    def create_container_client(self, connection_string : str, container_name : str):
         container_client = ContainerClient.from_connection_string(connection_string, container_name)
         return container_client, container_name 
+    
+    def create_blob_container(container_client : ContainerClient, container_name : str): 
+
+        try: 
+            container_client.create_container() 
+            print(f"Container {container_name} created")
+
+        except ResourceNotFoundError:
+            print(f'Container {container_name} not found.')
+            raise ResourceNotFoundError 
+    
+        except ResourceExistsError:
+            print(f'Container {container_name }already exists')
+            raise ResourceExistsError
 
     def retrieve_blob_client(
             self, 
